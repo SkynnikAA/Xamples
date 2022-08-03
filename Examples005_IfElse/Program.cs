eaafe9ab94e9c54936e66293b28172fd8489fe7b
SkynnikAA <devid99@yandex.ru>
2022-08-03 18:33:43 +0500

    новый проэкт имя

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine( );
+
+if(username.ToLower() == "саша")
+{
+Console.WriteLine("Ура, это же Александр!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
