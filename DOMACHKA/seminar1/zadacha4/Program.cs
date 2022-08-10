1b46b83522a834bdcbd90fc2567696d46d92e5b4
SkynnikAA <devid99@yandex.ru>
2022-08-10 16:41:41 +0500

    выполнил задачи семинара 1 по С шарп

---
+++ DOMACHKA/seminar1/zadacha4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+    if (b > max)
+        {
+            max = b;
+        }
+
+        if (c > max)
+        {
+            max = c;
+        }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+            
